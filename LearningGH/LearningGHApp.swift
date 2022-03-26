8da82846c7fbc371b6b2d9cbdb51d539e5095caa
Aris Sarkisian <arissarkisian1651@gmail.com>
2022-03-25 23:21:31 -0700

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Aris Sarkisssian on 3/25/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
